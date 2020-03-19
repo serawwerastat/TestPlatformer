c543fe557870fae47e9951bbf98a5ab39b935f35
Artur Filatov <serawwerastat@gmail.com>
2020-03-19 21:01:57 +0200

    Finish level 5

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -61,8 +61,8 @@ namespace DefaultNamespace
         private const float LevelWorstTime1 = 100f;
         private const float LevelWorstTime2 = 125f;
         private const float LevelWorstTime3 = 300f;
-        private const float LevelWorstTime4 = 10f;
-        private const float LevelWorstTime5 = 10f;
+        private const float LevelWorstTime4 = 350f;
+        private const float LevelWorstTime5 = 425f;
         private const float LevelWorstTime6 = 10f;
         private const float LevelWorstTime7 = 10f;
         private const float LevelWorstTime8 = 10f;
